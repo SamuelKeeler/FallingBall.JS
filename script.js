2980238aa320191cb34a99a3d086baf303e74d72
Methlor <keelersamuel@gmail.com>
2020-10-22 16:28:26 +0300

    Working Game, Will Changes Styling on next Commit & Add Giff

--- script.js
+++ script.js
@@ -1,52 +1,117 @@
 var character = document.getElementById("character");
 var game = document.getElementById("game");
 var interval;
-var both= 0;
+var both = 0;
 var counter = 0;
+var currentBlocks = [];
 
+
+// Movment
 function moveLeft(){
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     if(left>0){
-    character.style.left + left + 2 - "px";
+        character.style.left = left - 2 + "px";
     }
 }
-
 function moveRight(){
-    var left = parseInt(window.getComputedStyle(character).getPropertyValue("right"));
-    if(right>380){
-    character.style.right + right + 2 + "px";
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left<380){
+        character.style.left = left + 2 + "px";
     }
 }
 
 document.addEventListener("keydown", event => {
-    if (both==0){
+    if(both==0){
         both++;
         if(event.key==="ArrowLeft"){
-        Interval = setInterval(moveLeft, 1);
+            interval = setInterval(moveLeft, 1);
         }
         if(event.key==="ArrowRight"){
-        Interval = setInterval(moveRight, 1);
+            interval = setInterval(moveRight, 1);
         }
     }
 });
-
-document.addEventListener("keyup", event =>{
-    clearInterval(Interval);
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
     both=0;
 });
 
-setInterval(function(){
-    var block = document.createElement("div");
-    var hole = document.createElement("div");
-    block.setAttribute("class","block");
-    hole.setAttribute("class","hole");
-    var random=Math.floor(Math.random()* 360);
-    hole.style.left=random + "px";
+var blocks = setInterval(function(){
+    var blockLast = document.getElementById("block"+(counter-1));
+    var holeLast = document.getElementById("hole"+(counter-1));
+    
+    // Counter to spawn "Rows"
+    if (counter>0){
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
+        var holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
+    }
 
-    block.setAttribute("id","block"+counter);
-    hole.setAttribute("id","hole"+counter);
+    // Spawner & Memory Leak Control
+    if(blockLastTop < 400 || counter == 0 ){
+        
+        var block = document.createElement("div");
+        var hole = document.createElement("div");
+        block.setAttribute("class","block");    
+        hole.setAttribute("class","hole");
 
-    game.appendChild(block);
-    game.appendChild(hole);
-    counter++;
+        // Skip first Counter
+        block.style.top = blockLastTop + 100 +"px";
+        hole.style.top = holeLastTop + 100 +"px";
+
+        // Hole Spawner
+        var random=Math.floor(Math.random()* 360);
+        hole.style.left=random + "px";
+
+        block.setAttribute("id","block"+counter);
+        hole.setAttribute("id","hole"+counter);
+
+        game.appendChild(block);
+        game.appendChild(hole);
+        currentBlocks.push(counter);
+        counter++;
+    }   
+    
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var characterLeft = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var drop = 0;
+
+    if(characterTop <= 0){
+        alert("Game over. Score: "+(counter-9));
+        clearInterval(blocks);
+        location.reload();
+    }
+
+    // Moving Blocks
+    for(var i = 0; i < currentBlocks.length;i++){
+        let current = currentBlocks[i];
+        let ihole = document.getElementById("hole"+current);
+        let iblock = document.getElementById("block"+current);
+        let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
+        let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
+        iblock.style.top = iblockTop - 0.5 + "px";
+        ihole.style.top = iblockTop - 0.5 + "px";
+
+        // Memory Leak Control (Deletes Blocks that pass the top block value)
+        if(iblockTop < -20 ){
+            currentBlocks.shift();
+            iblock.remove();
+            ihole.remove();
+        }
+        if(iblockTop-20<characterTop && iblockTop>characterTop){
+            drop++;
+            if(iholeLeft<= characterLeft && iholeLeft+20>characterLeft){
+                drop = 0;
+            }
+        }
+    }
+
+    if(drop==0){
+        if(characterTop < 480){
+            character.style.top = characterTop + 2 + "px";
+        }
+    }else{
+        character.style.top = characterTop - 0.5 + "px";
+    }
 },1);
